5c20b33e0e81a6141e425187132386f215928abf
Badhon-Sarker <badhonsarker38@gmail.com>
2024-08-18 15:56:01 +0600

    navbar added

---
+++ src/Routes/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+            home bro
+            
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Routes/Root/Root.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../../Components/Navbar/Navbar";
+
+
+const Root = () => {
+    return (
+        <div className="px-2 mx-auto">
+            <Navbar></Navbar>
+           <Outlet></Outlet>
+            
+        </div>
+    );
+};
+
+export default Root;
