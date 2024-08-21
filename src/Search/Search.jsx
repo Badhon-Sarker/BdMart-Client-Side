744397e14d6807be131d8f26f8a5d2113ea66895
Badhon-Sarker <badhonsarker38@gmail.com>
2024-08-21 17:36:37 +0600

    search added

--- src/Search/Search.jsx
+++ src/Search/Search.jsx
@@ -1,25 +1,32 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ProductCard from "../Components/ProductCard/ProductCard";
 
 const Search = () => {
   const { query } = useParams();
   const [result, setResult] = useState([]);
-  console.log(result)
 
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_URL}/Search?search=${query}`)
       .then((res) => {
         setResult(res.data);
-        console.log(res.data);
       })
       .catch((err) => {
         console.error(err);
       });
   }, [query]);
 
-  return <div></div>;
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {result?.map((item) => (
+          <ProductCard key={item._id} item={item}></ProductCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
